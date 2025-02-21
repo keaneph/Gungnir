d96c5fc7dbbb9c0d48ed98853a71ce67bdc5cc66
keaneph <nearpharelle@gmail.com>
2025-02-21 23:25:00 +0800

    added history view

---
+++ Views/HistoryView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace sis_app.Views
+{
+    public partial class HistoryView : UserControl
+    {
+        public HistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
