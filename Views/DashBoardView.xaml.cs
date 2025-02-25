32a2d065152fc49db2bfdcf58d0dfb639c50e952
keaneph <nearpharelle@gmail.com>
2025-02-26 00:38:05 +0800

    chore: added detailed comments to about, dashboard, and history

--- Views/DashBoardView.xaml.cs
+++ Views/DashBoardView.xaml.cs
@@ -2,8 +2,10 @@
 
 namespace sis_app.Views
 {
+    // dashboard view code-behind class
     public partial class DashboardView : UserControl
     {
+        // constructor initializes dashboard components
         public DashboardView()
         {
             InitializeComponent();
--- Views/HistoryView.xaml.cs
+++ Views/HistoryView.xaml.cs
@@ -2,8 +2,10 @@
 
 namespace sis_app.Views
 {
+    // history view code-behind class
     public partial class HistoryView : UserControl
     {
+        // constructor initializes historyview components
         public HistoryView()
         {
             InitializeComponent();
